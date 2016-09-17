68fccf153a4547a183e4700b483b6725ea967b50
Davi <davijff@gmail.com>
2016-09-17 11:08:07 -0300

    Conclusão do Caso de Teste CadastrarEmpresa

---
+++ sceweb/src/br/sceweb/teste/UC01CadastrarEmpresa.java
@@ -0,0 +1,59 @@
+package br.sceweb.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.sceweb.modelo.Empresa;
+import br.sceweb.modelo.EmpresaDAO;
+
+public class UC01CadastrarEmpresa {
+
+	static Empresa empresa;
+	static EmpresaDAO empresaDAO;
+
+	/**
+	 * estabelece as pré condições de teste
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		empresaDAO = new EmpresaDAO();
+		empresa = new Empresa();
+		empresa.setNomeDaEmpresa("empresa x");
+		empresa.setCnpj("89424232000180");
+		empresa.setNomeFantasia("empresa x");
+		empresa.setEndereco("rua taquari");
+		empresa.setTelefone("2222");
+	}
+
+	@Before
+	public void excluiCNPJ() {
+		empresaDAO.exclui("89424232000180");
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		
+	}
+
+	/**
+	 * verificar o comportamento do sistema na inclusão de uma empresa com
+	 * sucesso
+	 */
+	@Test
+	public void CT01UC01FBCadastrarEmpresa_com_sucesso() {
+		assertEquals(1, empresaDAO.adiciona(empresa));
+	}
+	
+	/**
+	 * verificar o comportamento do sistema na inclusão de uma empresa com
+	 * cnpj inválido
+	 */
+	@Test(expected=RuntimeException.class)
+	public void CT02UC01FBCadastrarEmpresa_com_cnpj_invalido() {
+		empresaDAO.adiciona(empresa);
+	}
+}
